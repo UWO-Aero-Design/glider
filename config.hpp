21d014ce2adbfa13f360d140dac0bcca896a70e6
zacharycchen <zachary.chen00@gmail.com>
2020-10-10 18:40:40 -0400

    Creation of classes for each peripheral.

---
+++ config.hpp
@@ -0,0 +1,10 @@
+//File for constants and other parameters
+
+#pragma once
+
+//All the includes for each sub class ie. sensors and servo classes etc
+
+
+
+
+//Constants here
---
+++ glider_main.ino
@@ -0,0 +1,19 @@
+#include "Arduino.h"
+#include "config.hpp"
+
+
+void setup() {
+
+  //Create instances for each class
+
+
+}
+
+void loop() {
+
+  //Do stuff that makes the glider fly
+
+
+  
+
+}
